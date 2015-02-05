b598dc87e9c51ae42dfb8601fac65920d1826c51
Andy Gill <andygill@ku.edu>
2015-02-05 02:38:42 -0600

    Adding cabal

--- src/Control/Natural.hs
+++ src/Control/Natural.hs
@@ -1,8 +1,21 @@
-{-# LANGUAGE GADTs, RankNTypes, KindSignatures, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, TypeOperators #-}
+{-# LANGUAGE GADTs, RankNTypes, KindSignatures, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 module Control.Natural where
 
-import Control.Transformation
 import qualified Control.Category as C
+
+class Transformation t f g | t -> f, t -> g where
+  -- | The invoke method for a natural transformation.
+  -- Notice there is no mention of monads or functors here.
+  (#) :: t -> f a -> g a
+   
+-- Free theorem for natural transformations: fmap h . r == r . fmap h, 
+-- (From Bartosz Milewski's blog, Functional Pearl: F for Functor, and other places)
+-- Sometimes t is called a functor morphism.
+
+{-# RULES "natural free theorem" [~] 
+    forall h (r :: (Functor f, Functor g, Transformation t f g) => t) . 
+    fmap h . (r #) = (r #) . fmap h 
+  #-}
 
 -- Originally called Natural.
 newtype Natural :: (* -> *) -> (* -> *) -> * where
