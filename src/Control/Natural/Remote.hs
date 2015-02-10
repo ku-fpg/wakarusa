cffb6a2e3fecef24f97c8775a2f09c1436ae706d
Andy Gill <andygill@ku.edu>
2015-02-10 00:40:41 -0600

    Start the NT idea working with AFs

--- src/Control/Natural/Remote.hs
+++ src/Control/Natural/Remote.hs
@@ -1,8 +1,14 @@
-{-# LANGUAGE GADTs, RankNTypes, KindSignatures, MultiParamTypeClasses, FlexibleInstances, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings, GADTs, ScopedTypeVariables, RankNTypes, KindSignatures, MultiParamTypeClasses, FlexibleInstances, GeneralizedNewtypeDeriving #-}
 module Control.Natural.Remote where
         
 import Control.Natural
 import Data.Aeson as A
+import Data.Text(Text)
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector as Vector
+
+import Control.Monad.ConstrainedNormal
+
 
 {-
 instance Remote f where
@@ -10,24 +16,77 @@ instance Remote f where
   
 data Request :: * -> * where
   SyncMsg :: () -> Request ()
-
-data Square :: * -> * where
- Square :: Int -> Square Int      -- (remotely) square a number
  
 -}
+----------------------------------------------------------------------------------------------------
+data Square :: * -> * where
+ Square :: Int -> Square Int      -- (remotely) square a number
+
+-- {"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
+
+encodeSquare :: Square a -> JsonRpcRequest
+encodeSquare (Square n) = ("square", [Number $ fromIntegral n])
+
+decodeSquare :: forall a . Square a -> JsonReqResponse -> Maybe a
+decodeSquare (Square {}) (Number n) = return (4 :: Int)
+
+square :: Int -> NAF Unconstrained Square Int
+square = liftNAF . Square
+
+runSquare :: Natural JsonRpc2 IO -> Natural (NAF Unconstrained Square) IO
+runSquare jsonRpc = Natural $ \ f -> do
+   undefined
+{-
+   let en = encodeSquare f
+   [r] <- jsonRpc # JsonRpc2 [en]
+   case (decodeSquare f r) of
+     Just a -> return a
+-}
+-- runSquare runJsonRpc # Square 4
+theSquare :: Natural (NAF Unconstrained Square) IO
+theSquare = runSquare theJsonRpc2
+
+----------------------------------------------------------------------------------------------------
+
+data JsonRpc2 :: * -> * where
+ JsonRpc2 :: [JsonRpcRequest] -> JsonRpc2 [JsonReqResponse]
+
+type JsonRpcRequest = (Text,[Value])
+type JsonReqResponse = Value
+
+runJsonRpc2 :: Natural JsonRpc IO -> Natural JsonRpc2 IO
+runJsonRpc2 rpc = Natural $ \ f -> do
+    case f of
+      JsonRpc2 reqs -> sequence 
+              [ do v <- rpc # (Send $ Object $ HashMap.fromList 
+                              [ ("method",String method)
+                              , ("params",Array $ Vector.fromList $ args)
+                              ])
+                   case v of
+                    Just (Object v) -> return $ (v HashMap.! "response")
+              | (method,args) <- reqs 
+              ]
+
+theJsonRpc2 :: Natural JsonRpc2 IO
+theJsonRpc2 = runJsonRpc2 theJsonRpc
+
+----------------------------------------------------------------------------------------------------
+
+-- Layers: <user-facing>,<protocol-specific>,<generic>,ByteStream
 
 data JsonRpc :: * -> * where
  Send :: Value -> JsonRpc (Maybe Value)
 
-runJsonRpc :: Natural JsonRpc IO
-runJsonRpc = Natural $ \ f ->
+theJsonRpc :: Natural JsonRpc IO
+theJsonRpc = Natural $ \ f ->
   case f of
-    Send v -> do
-            print ("->",v)
-            let v' = v
-            print ("<-",v')
-            return (Just v')
+    Send (Object o) -> do
+            print ("->" :: String,o)
+            let o' = HashMap.insert "response" (Number 99) o
+            print ("<-" :: String,o')
+            return (Just (Object o'))
     
+--  runJsonRpc # Send (Number 9)
 
 {-
 newtype Remote f = Remote (Natural f IO)
