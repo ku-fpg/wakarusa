72c6ea2a841c84454f918ead69e373a9a0db466d
Andy Gill <andygill@ku.edu>
2015-02-14 12:08:17 -0600

    Abstract the Session

--- src/Control/Natural/Session.hs
+++ src/Control/Natural/Session.hs
@@ -6,10 +6,10 @@ import Data.ByteString (ByteString)
 import Control.Natural (Natural)
 
 -- | The 'Session' is our interface for sending and receiving ByteString messages over a network.
--- Clients build on top of 'Natural' 'Session' m; Servers build 'Natural' 'Session' m.
+-- Clients build on top of 'Natural' ('Session' 'ByteString') m; Servers build 'Natural' ('Session' 'ByteString') m.
 
-data Session :: * -> * where
-  Send  :: ByteString -> Session (Maybe ByteString) -- Messages that have a reply
-  Send_ :: ByteString -> Session ()                 -- Messages that do not need reply
-  Close ::               Session ()                 -- Last action; can free session resourses.
+data Session :: * -> * -> * where
+  Send  :: msg -> Session msg (Maybe msg) -- Messages that have a reply
+  Send_ :: msg -> Session msg ()                 -- Messages that do not need reply
+  Close ::        Session msg ()                 -- Last action; can free session resourses.
 
