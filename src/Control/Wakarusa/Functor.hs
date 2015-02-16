150c372604233740881b644834eba11aaa607c30
Andy Gill <andygill@ku.edu>
2015-02-16 14:54:08 -0600

    Adding MONAD type

--- src/Control/Wakarusa/Functor.hs
+++ src/Control/Wakarusa/Functor.hs
@@ -5,14 +5,27 @@ import Control.Monad.ConstrainedNormal
 
 import Control.Natural
 
+---------------------------------------------------------------------------
+
+class Functor1 h where
+ fmap1 :: Natural f g -> Natural (h f) (h g)
+
+---------------------------------------------------------------------------
 type FUNCTOR = NF Unconstrained
 
 nf :: Natural m (FUNCTOR m)
 nf = Natural liftNF
 
-class Functor1 h where
- fmap1 :: Natural f g -> Natural (h f) (h g)
-
 instance Functor1 (NF c) where
  fmap1 o = Natural $ foldNF (\ x_a tx -> fmap x_a (liftNF (o # tx)))
+
+---------------------------------------------------------------------------
+type MONAD = NM Unconstrained
+
+nm :: Natural m (MONAD m)
+nm = Natural liftNM
+
+instance Functor1 (NM c) where
+  fmap1 o = Natural $ foldNM return (\ tx x_r -> liftNM (o # tx) >>= x_r)
+
 
